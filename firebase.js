9fd2fabafbcc9db032d8039b78ce07e489950f7a
luisatapiero <79235499+luisatapiero@users.noreply.github.com>
2023-05-19 08:59:36 -0500

    auth state changed and profile pic

--- firebase.js
+++ firebase.js
@@ -1,76 +1,130 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, getDocs, addDoc, doc, setDoc, updateDoc, deleteDoc } from "firebase/firestore";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import { getStorage, ref, uploadBytes, getDownloadURL } from "firebase/storage";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAdNjiblO1Yv9a8kiCY3mB7nIUo5TpnRc8",
-    authDomain: "to-do-list-luisatapiero.firebaseapp.com",
-    projectId: "to-do-list-luisatapiero",
-    storageBucket: "to-do-list-luisatapiero.appspot.com",
-    messagingSenderId: "635925377487",
-    appId: "1:635925377487:web:469ff2735aef522963a2ba"
+  apiKey: "AIzaSyAdNjiblO1Yv9a8kiCY3mB7nIUo5TpnRc8",
+  authDomain: "to-do-list-luisatapiero.firebaseapp.com",
+  projectId: "to-do-list-luisatapiero",
+  storageBucket: "to-do-list-luisatapiero.appspot.com",
+  messagingSenderId: "635925377487",
+  appId: "1:635925377487:web:469ff2735aef522963a2ba"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
+const storage = getStorage(app);
 
 export async function getTasks() {
 
-    const allTasks = []
+  const allTasks = []
 
-    const querySnapshot = await getDocs(collection(db, "tasks"));
-    querySnapshot.forEach((doc) => {
-        //console.log(`${doc.id} => ${doc.data()}`);
-        allTasks.push({ ...doc.data(), id: doc.id })
-    });
+  const querySnapshot = await getDocs(collection(db, "tasks"));
+  querySnapshot.forEach((doc) => {
+    //console.log(`${doc.id} => ${doc.data()}`);
+    allTasks.push({ ...doc.data(), id: doc.id })
+  });
 
-    return allTasks
+  return allTasks
 }
 
 export async function addTask(taskTitle) {
-    try {
-        const docRef = await addDoc(collection(db, "tasks"), {
-            title: taskTitle,
-        });
-        console.log("Document written with ID: ", docRef.id);
-    } catch (e) {
-        console.error("Error adding document: ", e);
-    }
+  try {
+    const docRef = await addDoc(collection(db, "tasks"), {
+      title: taskTitle,
+    });
+    console.log("Document written with ID: ", docRef.id);
+  } catch (e) {
+    console.error("Error adding document: ", e);
+  }
+
+}
+
+export async function addUserToDb(userInfo, id) {
+  try {
+    await setDoc(doc(db, "users", id), userInfo);
+
+    console.log("User written with ID: ", id);
+  } catch (e) {
+    console.error("Error adding user: ", e);
+  }
 
 }
 
 export async function editDocument(title, id, completed) {
-    const docRef = doc(db, "tasks", id);
-  
-    try {
-      await updateDoc(docRef, {
-        title: title,
-        completed: !completed // Cambia el estado de completado al valor opuesto
-      });
-      console.log("Document updated successfully");
-    } catch (e) {
-      console.error("Error updating document: ", e);
-    }
-  }
-  
-  
-  
+  const docRef = doc(db, "tasks", id);
 
-  export async function deleteDocument(id) {
-    const docRef = doc(db, "tasks", id);
-  
-    try {
-      await deleteDoc(docRef);
-      console.log("Document deleted successfully");
-    } catch (e) {
-      console.error("Error deleting document: ", e);
-    }
+  try {
+    await updateDoc(docRef, {
+      title: title,
+      completed: !completed // Cambia el estado de completado al valor opuesto
+    });
+    console.log("Document updated successfully");
+  } catch (e) {
+    console.error("Error updating document: ", e);
   }
-  
-  
+}
 
+
+
+
+export async function deleteDocument(id) {
+  const docRef = doc(db, "tasks", id);
+
+  try {
+    await deleteDoc(docRef);
+    console.log("Document deleted successfully");
+  } catch (e) {
+    console.error("Error deleting document: ", e);
+  }
+}
+
+export async function createUser(userInfo) {
+
+
+  try {
+    // Signed in 
+    const userCredential = await createUserWithEmailAndPassword(auth, userInfo.email, userInfo.pass)
+    const user = userCredential.user;
+    console.log(user)
+    // ...
+
+    //subir imagen
+    const url = await uploadFile(user.uid + userInfo.picture.name, userInfo.picture, 'profilePictures')
+
+    //crear usuario DB
+    const dbInfo = {
+      url,
+      email: userInfo.email,
+      birthday: userInfo.birthday,
+      username: userInfo.username
+    }
+    addUserToDb(dbInfo, user.uid)
+  }
+  catch (error) {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    alert(error.message)
+  }
+}
+
+export async function uploadFile(name, file, folder) {
+  try {
+    const taskImgRef = ref(storage, `${folder}/${name}`);
+
+    await uploadBytes(taskImgRef, file);
+    const url = await getDownloadURL(taskImgRef);
+    return url;
+  } catch (error) {
+    console.log("error creando imagen ->", error);
+  }
+}
 
 
---
+++ sign-up/sign-up.js
@@ -0,0 +1,30 @@
+import './style.css'
+import { createUser } from '../firebase.js'
+
+const inputElements = document.querySelector('#sign-up-form').querySelectorAll('input')
+console.log(inputElements)
+const formButton = document.getElementById('form-button')
+formButton.addEventListener('click',(e)=>signUp(e))
+
+function signUp(e){
+    e.preventDefault()
+
+    const userInfo = {}
+        inputElements.forEach((elem)=>{
+            if(elem.files && elem.files[0]){
+                userInfo[elem.name] = elem.files[0]
+            }else if(elem.value && elem.value.length > 0){
+                userInfo[elem.name] = elem.value
+            }else{
+                alert('No todos los valores están diligenciados')
+            }
+        })
+
+        console.log(userInfo)
+        if (userInfo.pass === userInfo.confirm){
+            createUser(userInfo)
+        }else{
+            alert('Las contraseñas no coinciden')
+        }
+    
+}
